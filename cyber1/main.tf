f3fa94c8525f9567c7655fd30b9cfcab5beadd98
DiogoCarreiroRego <diogocarreirorego@enta.pt>
2023-03-17 11:14:22 -0100

    17.03.2023

--- cyber1/main.tf
+++ cyber1/main.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "CyberSecurity" {
   key_name   = "Redes"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC2W6MG+b8k9HKNMRyQksi+S2XBryBNK9uj4DkXu542Hf4hd32zCFnlNbik3EDOE8EKeg8S6IvJGdTeBLYraxize2tm5DMGz6LMeWmyK+OK/EDxYtRpJMXC7Dk3rbRdsRZyvqqmQmP5hJEV5A0zLUYwgm6cIxGa1a+671AG2cX170qbmdb3OzajlccaEIq0QexBoBM2kDpu3kG4aeLKShbp9Pjj290tRFvVMEqwi8Ay8KdQY8Sg1f8QTdJe9F5icACm4SLkfTiSw0sB7nDInNj3R4/Q7yn4Hx65K5uG8jXUGTsHdWcFgrm/naWJ3yfxoNOQayZlX44lL4rsiy7w/HPZR2MBFcYAJxfDG0Pam6fXnQr7MHZ/XK0QaP2vvPbmlifPaJQMEAfOvZxhRS7CLmFawbqokxsO+62ADB/38ycxD0Hq2Q1LF9+f4f17Tq8JRssYFkdzM63l0tnQhbyZM6g+J3VMgovSHu3jBHLZ9wIhilndQvQNtecIqVGzGposdBp0c/USF4km12xk/RXsQnWnbp+8fNcdiSF3dkF0p+dIdfHIuTH5GnmzP9187K3FZu0gueQJuU5Lx0IP+6TEZgSrDJgElx6PJCPzIv6/2OL3QdmhzwcFN3L/xq0GF4GMGxeGGouVQjRKo+Q3u3PLezVoAgAlnAd3qAikP82VA2358w== "
+  public_key = var.public_key
 }
 
 resource "aws_vpc" "CyberSecurity" {
@@ -172,16 +172,6 @@ resource "aws_security_group" "cyber_default" {
     "Name" = "CyberSecurity"
   }
   vpc_id      = aws_vpc.CyberSecurity.id
-}
-
-resource "aws_vpc_security_group_ingress_rule" "cyber_home" {
-  cidr_ipv4              = "128.65.243.205/32"
-  description            = "Home"
-  ip_protocol            = "-1"
-  security_group_id      = aws_security_group.cyber_default.id
-  tags                   = {
-    "Name" = "Home IP address"
-  }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "cyber_nos_enta" {
